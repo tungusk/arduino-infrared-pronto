1c7c5aeb95468e20c49930e67bb34d764d3b8b89
probonopd <probono@puredarwin.org>
2013-07-06 23:34:24 +0200

    Update IR_Player_ProntoCode.c

--- IR_Player_ProntoCode.c
+++ IR_Player_ProntoCode.c
@@ -31,207 +31,187 @@
  */
 
 
-/* Includes ------------------------------------------------------------------*/
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdbool.h>
+/*
+Send Pronto Hex via an IR LED connected to Arduino Pin D9.
+ Make sure you do not send a blank (" ") as the last character.
+ Send the following command over the serial line:
  
-/* Private define ------------------------------------------------------------*/
-#define F_CPU           16000000        // System clock frequency
-#define LED_PIN         _BV(1)          // OC1A -> PB1 = Arduino Digital Pin 9 
-#define DEBUG_D13       _BV(5)          // GPIO -> PB5 = Arduino Digital Pin 13
+ # Sony12, device = 1, obc = 47
+ SEND 0000 0067 0000 000d 0060 0018 0030 0018 0030 0018 0030 0018 0030 0018 0018 0018 0030 0018 0018 0018 0030 0018 0018 0018 0018 0018 0018 0018 0018 03de
+ 
+ or
+ 
+ # RC5, device = 11, obc = 64
+ SEND 0000 0073 0000 000B 0040 0020 0020 0020 0020 0040 0040 0040 0020 0020 0040 0020 0020 0020 0020 0020 0020 0020 0020 0020 0020 0CC8
+ 
+ Based on https://github.com/stephenong/Arduino-IR-Remote-Control-Player
 
-#define PRONTO_IR_SOURCE            0   // Pronto code byte 0
-#define PRONTO_FREQ_CODE            1   // Pronto code byte 1
-#define PRONTO_SEQUENCE1_LENGTH     2   // Pronto code byte 2
-#define PRONTO_SEQUENCE2_LENGTH     3   // Pronto code byte 3
-#define PRONTO_CODE_START           4   // Pronto code byte 4
-
-#define TOTAL_CYCLES 400000             // Turns off after this number of 
-                                        // cycles. About 10 seconds
-/* Hardware ------------------------------------------------------------------*/
-
-//  Platform - Arduino Deumilanove - ATMEGA328p - 16MHz
-//  
-//  Connect IR LED anode to Arduino Digital Pin 9 (PB1) and connect its 
-//  cathode to GND. 
-
-/* Pronto Code Format---------------------------------------------------------*/
-
-//  Reference:
-//    http://www.remotecentral.com/features/irdisp1.htm
-// 
-//  Decoding:
-//  1. First number is always 0 which means leared IR pattern.
-//  2. Second number: Carrier freq in Hz. Freq = 1000000/(N * .241246) 
-//  3. Third number: number of burst pair in non repeating burst pair #1
-//  4. Fourth number: number of burst pair in repeating bust pair #2
-//  5. Start of burst pairs. Burst pair #1 followed by burst pair #2
-//
-//  Eg Power off:
-//  0000 0067 0000 000d 
-//  0060 0018 
-//  0030 0018 
-//  0030 0018 
-//  ...
-//  ...
-//  0018 03de
-//
-//  Word 1: 0x0000
-//  Word 2: 0x0067 -> Carrier 40.244kHz
-//  Word 3: 0x0000 -> 0 non repeating burst pair
-//  word 4: 0x000d -> 13 repeating burst pair
-//  Word 5, 6: Burst pair 2 -> 0x0060 0x0018 -> 96 cycles on, 24 cycles off
-//  etc
+  * This work is licenced under the Creative Commons Attribution-NonCommercial 
+  * 3.0 Unported License. To view a copy of this licence, visit 
+  * http://creativecommons.org/licenses/by-nc/3.0/ or send a letter to Creative 
+  * Commons, 171 Second Street, Suite 300, San Francisco, California 94105, USA.
+  *
+  * <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/"><img alt="Creative Commons Licence" style="border-width:0" src="http://i.creativecommons.org/l/by-nc/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Infrared Remote Control Player</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://robotics.ong.id.au" property="cc:attributionName" rel="cc:attributionURL">Stephen Ong</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons Attribution-NonCommercial 3.0 Unported License</a>.
+*/
 
 
-/* Pronto Codes --------------------------------------------------------------*/
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <avr/pgmspace.h>
+#include <stdint.h>
+#include <avr/io.h>
+#define IR_PORT PORTB
+#define IR_PIN PINB
+#define IR_DDR DDRB
+#define IR_BV _BV(1)
+#define IR_OCR OCR1A
+#define IR_TCCRnA TCCR1A
+#define IR_TCCRnB TCCR1B
+#define IR_TCNTn TCNT1
+#define IR_TIFRn TIFR1
+#define IR_TIMSKn TIMSK1
+#define IR_TOIEn TOIE1
+#define IR_ICRn ICR1
+#define IR_OCRn OCR1A
+#define IR_COMn0 COM1A0
+#define IR_COMn1 COM1A1
+#define PRONTO_IR_SOURCE 0 // Pronto code byte 0
+#define PRONTO_FREQ_CODE 1 // Pronto code byte 1
+#define PRONTO_SEQUENCE1_LENGTH 2 // Pronto code byte 2
+#define PRONTO_SEQUENCE2_LENGTH 3 // Pronto code byte 3
+#define PRONTO_CODE_START 4 // Pronto code byte 4
 
-//Test Pronto Code Signal
-uint16_t ProntoCodeTest[] = {
-    0x00,                   //0 - recorded waveform
-    0x6D,                   //38kKhz. 0x67=>40kHz, 0x6D=>38kHz
-    0x01,                   //Sequence 1 on/off pair length
-    0x03,                   //Sequence 2 on/off pair length
-    0x05, 0x01,             //Sequence 1, pair 1
-    0x02, 0x05,             //Sequence 2, pair 1
-    0x04, 0x03,             //Sequence 2, pair 2
-    0x06, 0x07};            //Sequence 2, pair 3
+static const uint16_t *ir_code = NULL;
+static uint16_t ir_cycle_count = 0;
+static uint32_t ir_total_cycle_count = 0;
+static uint8_t ir_seq_index = 0;
+static uint8_t ir_led_state = 0;
 
-
-// Power Off - Sony LCD (2010) Television (Power Control) 
-// http://files.remotecentral.com/library/3-2/sony/lcd_%282010%29_television/index.html
-uint16_t ProntoCodeSonyOff[] = 
-    {0x00, 0x67, 0x00, 0x0d,
-     0x60, 0x18, 0x30, 0x18, 0x30, 0x18, 0x30, 0x18, 
-     0x30, 0x18, 0x18, 0x18, 0x30, 0x18, 0x18, 0x18, 
-     0x30, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18,
-     0x18, 0x3de };
-
-uint16_t *ProntoCode = ProntoCodeSonyOff;
-
-
-/* Private functions ---------------------------------------------------------*/
-
-/**************************************************************************//**
- * Turns IR LED on and off. This is done by connecting or disconnecting OCR1A
- * to physical pin. When connected, pin is pulsed at carrier frequency. When
- * disconnected pin works as GPIO, ie off.
- * @param on: LED is on
- * @retval None
- *****************************************************************************/
-
-uint8_t gIRLED_Power;       //True when IR led is on
-
-void IRLED_Power(uint8_t on) {
-    if (on) {
-        TCCR1A |= (1<<COM1A1) + (1<<COM1A0);
-        gIRLED_Power = true;
-        PORTB |= DEBUG_D13;  
-    }
-    else {
-        TCCR1A &=  ( (~(1<<COM1A1)) & (~(1<<COM1A0)) );
-        gIRLED_Power = false;
-        PORTB &= ~DEBUG_D13;
-    }
+void ir_on()
+{
+  IR_TCCRnA |= (1<<IR_COMn1) + (1<<IR_COMn0);
+  ir_led_state = 1;
 }
 
-/**************************************************************************//**
- * Toggles IR LED state.
- * @retval None
- *****************************************************************************/
-void IRLED_Toggle() {
-    if(gIRLED_Power) {
-        IRLED_Power(false);
-    }
-    else {
-        IRLED_Power(true);
-    }
+void ir_off()
+{
+  IR_TCCRnA &= ((~(1<<IR_COMn1)) & (~(1<<IR_COMn0)) );
+  ir_led_state = 0;
 }
 
-/**************************************************************************//**
- * ISR of timver1 overflow. This sequences the pronto code
- * @retval None
- *****************************************************************************/
-uint16_t gCycleCount;
-uint32_t gTotalCycleCount;
-uint8_t gCurrentSequenceIndex;
+void ir_toggle()
+{
+  if (ir_led_state)
+    ir_off();
+  else
+    ir_on();
+}
 
+void ir_start(uint16_t *code)
+{
+  ir_code = code;
+  IR_PORT &= ~IR_BV; // Turn output off
+  IR_DDR |= IR_BV; // Set it as output
+  IR_TCCRnA = 0x00; // Reset the pwm
+  IR_TCCRnB = 0x00;
+  //printf_P(PSTR("FREQ CODE: %hd\r\n"), code[PRONTO_FREQ_CODE]);
+  uint16_t top = ( (F_CPU/1000000.0) * code[PRONTO_FREQ_CODE] * 0.241246 ) - 1;
+  //printf_P(PSTR("top: %hu\n\r"), top);
+  IR_ICRn = top;
+  IR_OCRn = top >> 1;
+  IR_TCCRnA = (1<<WGM11);
+  IR_TCCRnB = (1<<WGM13) | (1<<WGM12);
+  IR_TCNTn = 0x0000;
+  IR_TIFRn = 0x00;
+  IR_TIMSKn = 1 << IR_TOIEn;
+  ir_seq_index = PRONTO_CODE_START;
+  ir_cycle_count = 0;
+  ir_on();
+  IR_TCCRnB |= (1<<CS10);
+}
+
+#define TOTAL_CYCLES 80000 // Turns off after this number of
+// cycles. About 2 seconds
+// FIXME: Turn off after having sent
 ISR(TIMER1_OVF_vect) {
-    uint16_t sequenceIndexEnd;          // Index to the last element in the 
-                                        // ProntoCode array
-    uint16_t repeatSequenceIndexStart;  // Index to ProntoCode array to repeat
+  uint16_t sequenceIndexEnd;
+  uint16_t repeatSequenceIndexStart;
+  ir_total_cycle_count++;
+  ir_cycle_count++;
 
-    gTotalCycleCount++;
-    gCycleCount++;
+  if (ir_cycle_count== ir_code[ir_seq_index]) {
+    ir_toggle();
+    ir_cycle_count = 0;
+    ir_seq_index++;
+    sequenceIndexEnd = PRONTO_CODE_START +
+      (ir_code[PRONTO_SEQUENCE1_LENGTH]<<1) +
+      (ir_code[PRONTO_SEQUENCE2_LENGTH]<<1);
 
-    // End of this state, toggle led and move to the next state
-    if (gCycleCount==ProntoCode[gCurrentSequenceIndex]) {
-        IRLED_Toggle();
-        gCycleCount = 0;
-        gCurrentSequenceIndex++;
-        sequenceIndexEnd = PRONTO_CODE_START +
-                           (ProntoCode[PRONTO_SEQUENCE1_LENGTH]<<1) +
-                           (ProntoCode[PRONTO_SEQUENCE2_LENGTH]<<1);
-                                    
-        repeatSequenceIndexStart = PRONTO_CODE_START +
-                                   (ProntoCode[PRONTO_SEQUENCE1_LENGTH]<<1);
+    repeatSequenceIndexStart = PRONTO_CODE_START +
+      (ir_code[PRONTO_SEQUENCE1_LENGTH]<<1);
 
-        // If index past last element in array, set index to repeat
-        if (gCurrentSequenceIndex >= sequenceIndexEnd ) {        
-            gCurrentSequenceIndex = repeatSequenceIndexStart;     
+    if (ir_seq_index >= sequenceIndexEnd ) {
+      ir_seq_index = repeatSequenceIndexStart;
 
-            if(gTotalCycleCount>TOTAL_CYCLES) {            // Finished
-                TCCR1B &= ~(1<<CS10);                    // Stop Timer
-                IRLED_Power(false);                        // Turn off LED
-            }
-        }
+      if(ir_total_cycle_count>TOTAL_CYCLES) {
+        ir_off();
+        TCCR1B &= ~(1<<CS10);
+      }
     }
+  }
 }
 
-/**************************************************************************//**
- * Main function. Setup gpio ports, timer1, then wait in infinite loop. The IR
- * code is played back for TOTAL_CYCLES cycles.
- * @retval none
- *****************************************************************************/
-int main (void){
-    PORTB = 0x00;                       // When LED_PIN disconnected from  
-                                        //    counter, LED will be in off state
-    DDRB = LED_PIN | DEBUG_D13;         // Set LED_PIN and DEBUG_D13 for output
-    
-                                        // SETUP TIMER 1    
-    TCCR1A = 0x00;                      // WGM=0000 for instant update of OCR1A
-    TCCR1B = 0x00;
-
-    uint16_t top = ( (F_CPU/1000000.0) * 
-                     ProntoCode[PRONTO_FREQ_CODE]  * 
-                     0.241246 ) - 1;
-
-    ICR1 = top;                         // Counter counts from 0 to top
-    OCR1A = top>>1;                     // PWM set to 50%, set OCR1A to top/2
-
-    
-    TCCR1A = (1<<WGM11);                // WGM = 1110 => Fast PWM, TOP=ICR1, 
-    TCCR1B = (1<<WGM13) | (1<<WGM12);   // TOV1 flag set on TOP, 
-                                        // OCR1x updated at BOTTOM
-
-    TCNT1 = 0x0000;                     // Clear counter
-    TIFR1 = 0x00;                       // Clear any pending interrupt
-    TIMSK1 = 1<<TOIE1;                   // Enable Overflow Interrupt
-
-    IRLED_Power(true);                  // IR Led starts with the ON state,
-                                        //   this connects OCR1A out to port pin
-    
-    gCurrentSequenceIndex = PRONTO_CODE_START;    
-    gCycleCount = 0;
-
-    
-    TCCR1B |= (1<<CS10);                // Start timer running, Clock = CLKio/1
-
-    sei();                              // Enable global interrupt
-
-    while (true) {}                     // Background loop does nothing
+void ir_stop()
+{
+  IR_TCCRnA = 0x00; // Reset the pwm
+  IR_TCCRnB = 0x00;
 }
 
-//---------------------------END OF IR_Player_ProntoCode.c -------------------
+const uint16_t inputLength = 512;
 
+void setup() {
+  Serial.begin(9600);
+  Serial.println("READY");
+}
+
+void loop()
+{
+  if ( Serial.available() > 0 )
+  {
+    static char input[inputLength];
+    static uint16_t i;
+    char c = Serial.read();
+
+    if ( c != '\r' && c != '\n' && i < inputLength-1)
+      input[i++] = c;
+
+    else
+    {
+      input[i] = '\0';
+      i = 0;
+
+      uint16_t array[80];
+      uint16_t j = 0;
+
+      if ( !strncmp(input, "SEND", 4) )
+      {
+        char* p = input+4;
+
+        while ( (p = strchr(p, ' ')) != NULL )
+          array[j++] = strtol(p, &p, 16);
+
+        ir_start(array);
+        Serial.print("SENT ");
+        for ( uint8_t i = 0; i < j; i++ )
+        {
+          Serial.print ("0x");
+          Serial.print (array[i], HEX);
+          Serial.print(" ");
+        }
+
+
+        Serial.println();
+      }
+    }
+  }
+}
